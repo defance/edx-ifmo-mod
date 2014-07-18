7be6fd042123a66d0fb424ff502a6dbd337f24f2
Dmitry Ivanyushin <defance@gmail.com>
2014-07-18 17:22:18 +0400

    Template lookup in studio fixed

--- ifmo_mod/startup.py
+++ ifmo_mod/startup.py
@@ -5,18 +5,19 @@ from .patch import (course, problem,)
 
 import edxmako
 
-import logging
-log = logging.getLogger(__name__)
-
 
 def patch_templates():
+    # TODO Add template path to template look up path collection in settings
     template_path = path(__file__).dirname() / 'templates'
     edxmako.paths.add_lookup('main', template_path, prepend=True)
+    if settings.SERVICE_VARIANT == 'cms':
+        edxmako.paths.add_lookup('lms.main', template_path, prepend=True)
 
 
 def _patch_variant_url(app_name, urls_module):
     app_urls = __import__('%s.urls' % (app_name,), fromlist=['urlpatterns'])
     app_urls.urlpatterns.insert(0, url(r'', include(urls_module)))
+
 
 def patch_urls():
     try:
