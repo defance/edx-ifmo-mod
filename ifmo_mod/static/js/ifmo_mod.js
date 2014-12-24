57ee0247dbcfc4654fa82a31c8fb03e5da100a2e
Dmitry Ivanyushin <defance@gmail.com>
2014-12-24 20:07:29 +0300

    Initialization code relocated

--- ifmo_mod/static/js/ifmo_mod.js
+++ ifmo_mod/static/js/ifmo_mod.js
@@ -1,26 +1,70 @@
-var Summary = {
+var Summary =
+{
 
-    render_course_table: function(data) {
+    latest_id: 0,
+
+    render_course_table: function(data)
+    {
         if (data.courses && data.courses.length) {
             $('#summary-content').html(_.template($('#summary-template-table').text())(data));
         }
     },
 
-    get_data: function() {
+    add_filter_date: function()
+    {
+        this.latest_id += 1;
+        $('.summary-filters-controls').before(
+                _.template($('#summary-template-date-field').text())({'id': this.latest_id})
+        );
+        $('#summary-date-picker-'+this.latest_id)
+                .datepicker({"dateFormat": "dd/mm/yy"})
+                .mask("99/99/9999")
+                .val(this.current_date());
+    },
+
+    remove_filter_date: function()
+    {
+        if (1 == this.latest_id) {
+            return;
+        }
+        $('.summary-filters-item-' + this.latest_id).remove();
+        this.latest_id -= 1;
+    },
+
+    current_date: function()
+    {
+        var d = new Date();
+        return d.getDate() + '/' + (d.getMonth()+1) + '/' + d.getFullYear();
+    },
+
+    reset_view: function()
+    {
+        $('#summary-content').html(_.template($('#summary-template-loading').text())());
+    },
+
+    filter_data: function()
+    {
+        this.reset_view();
+        var dates = [];
+        $('.summary-date-picker').each(function(e){
+            dates.push(this.value);
+        });
         $.ajax({
             url: '/summary_handler',
             dataType: 'json',
+            data: {'dates': dates},
             success: function(data) { Summary.render_course_table(data); }
-        })
+        });
     }
 
 };
 
 $(function() {
-    // Initialize date fields
-    $("#summary-date-picker-from").datepicker({"dateFormat": "dd/mm/yy"}).mask("99/99/9999");
-    $("#summary-date-picker-to").datepicker({"dateFormat": "dd/mm/yy"}).mask("99/99/9999");
 
-    // Render template
-    Summary.get_data();
+    $('#summary-button-add').click(function(e) {Summary.add_filter_date()});
+    $('#summary-button-remove').click(function(e) {Summary.remove_filter_date()});
+    $('#summary-button-filter').click(function(e) {Summary.filter_data()});
+
+    Summary.add_filter_date();
+    Summary.filter_data();
 });
