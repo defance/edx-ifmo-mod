e3cff40aa94568cdfbe849c3bde27b068fd17f65
Dmitry Ivanyushin <defance@gmail.com>
2014-12-26 18:10:57 +0300

    Register new users fixed encodings

--- ifmo_mod/management/commands/reg_new_users.py
+++ ifmo_mod/management/commands/reg_new_users.py
@@ -8,6 +8,7 @@ from opaque_keys.edx.keys import CourseKey
 from xmodule.modulestore.django import modulestore
 from xmodule.modulestore.exceptions import ItemNotFoundError
 
+import codecs
 
 class Command(BaseCommand):
 
@@ -26,6 +27,10 @@ class Command(BaseCommand):
                     dest='input', default=None,
                     help=("CVS file with following lines: "
                          "<sso_id>,<last_name>,<first_name>,<middle_name>,<birth_date>,<sex>,<country>")),
+        make_option('-o', '--output',
+                    dest='output', default=None,
+                    help=("CVS file with following lines: "
+                         "<id>,<sso_id>")),
         make_option('-c', '--course',
                     dest='course', default=None,
                     help="Course one of following: %s" % courses.keys()),
@@ -38,17 +43,17 @@ class Command(BaseCommand):
 
     @classmethod
     def get_sex_latin(cls, cyrillic):
-        if cyrillic == u'М':
-            return u'm'
-        elif cyrillic == u'Ж':
-            return u'f'
+        if cyrillic == 'М':
+            return 'm'
+        elif cyrillic == 'Ж':
+            return 'f'
         else:
-            return u''
+            return 'o'
 
     @classmethod
     def get_or_create_user(cls, source):
 
-        (sso_id, last_name, first_name, middle_name, birth_date, sex, country, _) = source.split(',', 7)
+        (sso_id, last_name, first_name, middle_name, birth_date, sex, country, _) = source.split(';', 7)
         registered = False
 
         try:
@@ -57,7 +62,8 @@ class Command(BaseCommand):
         except User.DoesNotExist:
             user = User.objects.create(
                 username=sso_id,
-                last_name=last_name, first_name=first_name, middle_name=middle_name
+                last_name=last_name, first_name=first_name,
+                email="%s@niuitmo.ru" % sso_id
             )
 
             profile = UserProfile.objects.create(
@@ -78,19 +84,26 @@ class Command(BaseCommand):
     def enroll_user(cls, user, course_str):
 
         course_key = CourseKey.from_string(course_str)
+        created = False
 
         try:
             course = modulestore().get_course(course_key)
         except ItemNotFoundError:
             raise CommandError("Course does not exist: %s" % course_str)
 
-        enrollment, created = CourseEnrollment.objects.get_or_create(
-            user=user,
-            course_id=course_key,
-            mode="honor",
-            is_active=True
-        )
-        enrollment.save()
+        try:
+            enrollement = CourseEnrollment.objects.get(
+                user=user,
+                course_id=course_key
+            )
+        except CourseEnrollment.DoesNotExist:
+            enrollement = CourseEnrollment.objects.create(
+                user=user,
+                course_id=course_key,
+                mode="honor",
+                is_active=True
+            )
+            created = True
 
         return created
 
@@ -100,13 +113,18 @@ class Command(BaseCommand):
             raise CommandError('Course is not specified.')
 
         if options.get('input') is None:
-            raise CommandError('Course is not specified.')
+            raise CommandError('Input file is not specified.')
+
+        if options.get('output') is None:
+            raise CommandError('Output file is not specified.')
 
         try:
-            with open(options.get('input'), 'r') as f:
+            with open(options.get('input'), 'r') as f, open(options.get('output'), 'w') as f2:
                 for line in f:
 
                     user, registered = self.get_or_create_user(line)
+                    f2.write("%s,%s\n" % (user.id, user.username))
+
                     if registered:
                         self.report['registered'] += 1
 
